08decc88af621ca57973d0c12bb842244d5b7ea4
FulkorRain <ranygamer353@gmail.com>
2025-11-29 18:26:28 +0300

    Added Basic Tower and Enemy Configs

--- Engine.cs
+++ Engine.cs
@@ -31,12 +31,39 @@ namespace TerminalTDS
         {
             choice = InputHelper.Ask("(A) Attack Menu. (B) Tower Menu. (C) Scout Enemy.", "a", "b", "c");
             ProcessMenu(choice);
+            Logger.Log("Choose Menu", "ShowMenu", "ProcessMenu", $"Player opened Main Menu and picked {choice}");
 
         }
 
         private void ProcessMenu(string input)
         {
-            
+            switch (input)
+            {
+                case "a":
+                    AttackMenu();
+                    break;
+                case "b":
+                    TowerMenu();
+                    break;
+                case "c":
+                    ScoutEnemyMenu();
+                    break;
+            }
+        }
+
+        private void AttackMenu()
+        {
+
+        }
+
+        private void TowerMenu()
+        {
+
+        }
+
+        private void ScoutEnemyMenu()
+        {
+
         }
     }
 }
--- GameConfig.cs
+++ GameConfig.cs
@@ -6,7 +6,102 @@ using System.Threading.Tasks;
 
 namespace TerminalTDS
 {
-    internal class GameConfig
+    internal static class GameConfig
     {
+        public static readonly Dictionary<string, TowerConfig> Tower = new()
+        {
+            ["archer"] = new TowerConfig
+            {
+                Name = "Flame-Arrow Archer",
+                Attack = 5,
+                Speed = 135,
+                level = 1,
+                Abilities = new List<TowerAbility>
+                {
+                    TowerAbility.None
+                }
+            },
+
+            ["mage"] = new TowerConfig
+            {
+                Name = "Frost Mage",
+                Attack = 3,
+                Speed = 98,
+                level = 1,
+                Abilities = new List<TowerAbility>
+                {
+                    TowerAbility.None
+                }
+            }
+        };
+
+        public static readonly Dictionary<string, EnemyConfig> Enemy = new()
+        {
+            ["goblin"] = new EnemyConfig
+            {
+                Name = "Goblin",
+                Health = 4,
+                Defense = 0,
+                Attack = 3,
+                Speed = 121,
+                level = 1,
+                Abilities = new List<EnemyAbility>
+                {
+                    EnemyAbility.None
+                }
+            },
+
+            ["orc"] = new EnemyConfig
+            {
+                Name = "Orc",
+                Health = 13,
+                Defense = 1,
+                Attack = 10,
+                Speed = 50,
+                level = 1,
+                Abilities = new List<EnemyAbility>
+                {
+                    EnemyAbility.None
+                }
+            }
+        };
+    }
+
+    internal class TowerConfig
+    {
+        public required string Name { get; set; }
+        public int Attack { get; set; }
+        public int Speed { get; set; }
+        public int level { get; set; }
+
+        public required List<TowerAbility> Abilities { get; set; }
+
+    }
+
+    public enum TowerAbility
+    {
+        None,
+    }
+
+    internal class EnemyConfig
+    {
+
+        public required string Name { get; set; }
+        public int Health { get; set; }
+        public int Defense { get; set; }
+        public int Attack { get; set; }
+        public int Speed { get; set; }
+        public int level { get; set; }
+
+        public required List<EnemyAbility> Abilities { get; set; }
+
+
+    }
+
+    public enum EnemyAbility
+    {
+        None,
+        TimedDeath,
+        OnDeath,
     }
 }
